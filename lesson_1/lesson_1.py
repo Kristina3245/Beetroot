34961775db9ef8e42d3a1800b6b49ce94b424209
Kristina Onishchenko <konys@softserveinc.com>
2023-04-28 20:03:57 +0300

    homework lesson 1 updated lesson_1.py

---
+++ lesson_1/lesson_1.py
@@ -0,0 +1,22 @@
+name="Kristina"
+day="Friday"
+print("Good day " + name +"! " + day + " is perfect day to learn some python." )
+
+
+name="Kristina"
+last_name="Onishchenko"
+print("Hello, " + name, last_name + "!")
+
+
+a=8
+b=3
+print(a+b)
+print(b-a)
+print(b/a)
+print(a*b)
+print(2**3)
+print(2%3)
+print(a//b)
+
+
+
